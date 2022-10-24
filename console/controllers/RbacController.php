4812baf67d541cde8e6a978684ce6f2eac3b1859
LOPERSON <73337926+LOPERSON@users.noreply.github.com>
2022-10-24 17:37:07 +0100

    BrunoRbac

--- console/controllers/RbacController.php
+++ console/controllers/RbacController.php
@@ -35,7 +35,52 @@ class RbacController extends Controller
         $deleteEvento = $auth->createPermission('deleteEvento');
         $deleteEvento->description = 'Delete a evento';
         $auth->add($deleteEvento);
-        
+
+        //Add Noticia permission
+        //Create
+        $createNoticia = $auth->createPermission('createNoticia');
+        $createNoticia->description = 'Create Noticia';
+        $auth->add($createNoticia);
+        //Update
+        $updateNoticia = $auth->createPermission('updateNoticia');
+        $updateNoticia->description = 'Update Noticia';
+        $auth->add($updateNoticia);
+        //Delete
+        $deleteNoticia= $auth->createPermission('deleteNoticia');
+        $deleteNoticia->description = 'Delete Noticia';
+        $auth->add($deleteNoticia);
+
+
+        //Add GestaoRp´s permission
+        //Create
+        $createRP = $auth->createPermission('createRP');
+        $createRP->description = 'Create RP';
+        $auth->add($createRP);
+        //Update
+        $updateRP = $auth->createPermission('updateRP');
+        $updateRP->description = 'Update RP';
+        $auth->add($updateRP);
+        //Delete
+        $deleteRP = $auth->createPermission('deleteEvento');
+        $deleteRP->description = 'Delete RP';
+        $auth->add($deleteRP);
+
+
+        //Add Bebida permission
+        //Create
+        $createBebida = $auth->createPermission('createBebida');
+        $createBebida->description = 'Create Bebida';
+        $auth->add($createBebida);
+        //Update
+        $updateBebida = $auth->createPermission('updateBebida');
+        $updateBebida->description = 'Update Bebida';
+        $auth->add($updateBebida);
+        //Delete
+        $deleteBebida = $auth->createPermission('deleteBebida');
+        $deleteBebida->description = 'Delete Bebida';
+        $auth->add($deleteBebida);
+
+
         //Add Comprar permission
         //Comprar
         $comprarPulseira = $auth->createPermission('comprarPulseira');
@@ -45,7 +90,7 @@ class RbacController extends Controller
     //----------------------------- Add Permission --------------------------------//
 
         $auth->add($gestor);
-        $auth->addChild($gestor, $createEvento, $updateEvento, $deleteEvento);
+        $auth->addChild($gestor, $createEvento, $updateEvento, $deleteEvento, $createBebida, $createNoticia, $createRP, $updateBebida, $updateNoticia, $updateRP, $deleteBebida, $deleteNoticia, $deleteRP);
 
         $auth->add($admin);
         $auth->addChild($admin, $gestor);
