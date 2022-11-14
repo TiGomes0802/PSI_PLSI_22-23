1b4491bf7f00bfc63bc4e153bfe36bde19010234
TiGomes0802 <tigomes08@gmail.com>
2022-11-14 16:23:39 +0000

    update frontend view userprofile
    delete create
    delete index
    delete search
    update view

--- frontend/views/userprofile/view.php
+++ frontend/views/userprofile/view.php
@@ -11,54 +11,59 @@ use yii\widgets\DetailView;
 $this->title = $model->nome . " " . $model->apelido;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="userprofile-view">
+<div class="row g-0">
+    <div class="col-2"></div>
+    <div class="col-8">
+        <div class="userprofile-view">
 
-    <h2><?= $model->nome . ' ' . $model->apelido ?></h2>
+            <h2><?= $model->nome . ' ' . $model->apelido ?></h2>
 
-    <br>
+            <br>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'label' => 'Nome',
-                'value' => function ($data) {
-                    return $data->nome .' '. $data->apelido;;
-                },
-            ],
-            [
-                'label' => 'Username',
-                'value' => function ($data) {
-                    return $data->user->username;
-                },
-            ],
-            [
-                'label' => 'Email',
-                'value' => function ($data) {
-                    return $data->user->email;
-                },
-            ],
-            [
-                'label' => 'Data de Nascimento',
-                'format' => ['date', 'php:d-m-Y'],
-                'value' => function ($data) {
-                    return $data->datanascimento;
-                },
-            ],
-            [
-                'label' => 'Sexo',
-                'value' => function ($data) {
-                    return $data->sexo;
-                },
-            ],
-        ],
-    ]) ?>
-
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->userid], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Update Password', ['update_password', 'id' => $model->userid], ['class' => 'btn btn-primary']) ?>
-    </p>
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    [
+                        'label' => 'Nome',
+                        'value' => function ($data) {
+                            return $data->nome .' '. $data->apelido;;
+                        },
+                    ],
+                    [
+                        'label' => 'Username',
+                        'value' => function ($data) {
+                            return $data->user->username;
+                        },
+                    ],
+                    [
+                        'label' => 'Email',
+                        'value' => function ($data) {
+                            return $data->user->email;
+                        },
+                    ],
+                    [
+                        'label' => 'Data de Nascimento',
+                        'format' => ['date', 'php:d-m-Y'],
+                        'value' => function ($data) {
+                            return $data->datanascimento;
+                        },
+                    ],
+                    [
+                        'label' => 'Sexo',
+                        'value' => function ($data) {
+                            return $data->sexo;
+                        },
+                    ],
+                ],
+            ]) ?>
 
 
+            <p>
+                <?= Html::a('Update', ['update', 'id' => $model->userid], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Update Password', ['update_password', 'id' => $model->userid], ['class' => 'btn btn-primary']) ?>
+            </p>
+
+
+        </div>
+    </div>
 </div>
