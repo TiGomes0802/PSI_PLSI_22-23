9b94d99d4ee9115f7cf617b14c679cddb3952bcb
LOPERSON <73337926+LOPERSON@users.noreply.github.com>
2022-12-12 16:57:14 +0000

    css

--- frontend/views/eventos/index.php
+++ frontend/views/eventos/index.php
@@ -1,52 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="pt">
+  <head>
+  <link rel="stylesheet" href="./../web/css/nicepage2.css" media="screen">
+<link rel="stylesheet" href="./../web/css/lista-de-eventos.css" media="screen">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+  </head>
+  <body class="u-body u-xl-mode" data-lang="pt">
+    <section class="u-clearfix u-custom-color-2 u-section-1" id="sec-07e1">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-align-center u-text u-text-default u-text-1" spellcheck="false">Lista de eventos</h2>
+        <div class="u-expanded-width-lg u-expanded-width-xl u-layout-grid u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            
+            <a href="https://link.com">
+              <div class="u-container-style u-custom-item u-image u-repeater-item u-shading" style="background-image: url('https://amymhaddad.s3.amazonaws.com/morocco-blue.png');">
+                <div class="u-container-layout u-similar-container u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-valign-top-xl u-container-layout-1">
+                  <h3 class="u-text u-text-default-xl u-text-2">Nome do evento</h3>
+                </div>
+              </div>
+            </a>
 
-use common\models\Eventos;
-use yii\helpers\Html;
-use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use yii\grid\GridView;
+            <a href="https://link.com">
+              <div class="u-container-style u-custom-item u-image u-repeater-item u-shading" style="background-image: url('https://amymhaddad.s3.amazonaws.com/morocco-blue.png');">
+                <div class="u-container-layout u-similar-container u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-valign-top-xl u-container-layout-1">
+                  <h3 class="u-text u-text-default-xl u-text-2">Nome do evento</h3>
+                </div>
+              </div>
+            </a>
 
-/** @var yii\web\View $this */
-/** @var common\models\EventosSearch $searchModel */
-/** @var yii\data\ActiveDataProvider $dataProvider */
+            <a href="https://link.com">
+              <div class="u-container-style u-custom-item u-image u-repeater-item u-shading" style="background-image: url('https://amymhaddad.s3.amazonaws.com/morocco-blue.png');">
+                <div class="u-container-layout u-similar-container u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-valign-top-xl u-container-layout-1">
+                  <h3 class="u-text u-text-default-xl u-text-2">Nome do evento</h3>
+                </div>
+              </div>
+            </a>
 
-$this->title = 'Eventos';
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="eventos-index">
+            <a href="https://link.com">
+              <div class="u-container-style u-custom-item u-image u-repeater-item u-shading" style="background-image: url('https://amymhaddad.s3.amazonaws.com/morocco-blue.png');">
+                <div class="u-container-layout u-similar-container u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-valign-top-xl u-container-layout-1">
+                  <h3 class="u-text u-text-default-xl u-text-2">Nome do evento</h3>
+                </div>
+              </div>
+            </a>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Eventos', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'nome',
-            'descricao',
-            'cartaz',
-            'dataevento',
-            //'numbilhetesdisp',
-            //'preco',
-            //'estado',
-            //'id_criador',
-            //'id_tipo_evento',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Eventos $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
-
-</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
