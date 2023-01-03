b2add9b9f74e51f59606eac39cf46ab1c903a049
LOPERSON <73337926+LOPERSON@users.noreply.github.com>
2023-01-03 16:49:49 +0000

    Id para os teste func

--- frontend/views/eventos/index.php
+++ frontend/views/eventos/index.php
@@ -19,7 +19,7 @@
             <?php 
               
             foreach ($eventos as $evento) { ?>
-              <a href='index.php?r=eventos%2Fview&id=<?= $evento->id ?>'>
+              <a href='index.php?r=eventos%2Fview&id=<?= $evento->id ?>' id="seleceevento">
               <div class="u-container-style u-custom-item u-image u-repeater-item u-shading" style="background-image: url('./../../backend/web/cartaz/<?= $evento->cartaz ?>');">
                 <div class="u-container-layout u-similar-container u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-valign-top-xl u-container-layout-1">
                   <h3 class="u-text u-text-default-xl u-text-2"><?= $evento->nome ?></h3>
