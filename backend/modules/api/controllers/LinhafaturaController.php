c037801dd002263b264ebb46321b3dd503cecc7c
Tiago Gomes <tigomes08@gmail.com>
2023-01-18 19:46:38 +0000

    Create LinhafaturaController.php
    Create one action actionViewalleventos

---
+++ backend/modules/api/controllers/LinhafaturaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace backend\modules\api\controllers;
+
+use Yii;
+use common\models\Faturas;
+use common\models\Pulseiras;
+use common\models\LinhaFatura;
+
+class LinhafaturaController extends \yii\web\Controller
+{
+    public function actionViewlinhafatura($id_fatura)
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $fatura = Faturas::findOne($id_fatura);
+        
+        $pulseira = Pulseiras::find()
+            ->where(['id' => $fatura->id_pulseira])
+            ->one();
+
+        if($pulseira->tipo == 'vip'){
+            $linhasfatura = LinhaFatura::find()
+                ->where(['id_fatura' => $fatura->id])
+                ->all();
+            
+            foreach($linhasfatura as $linhafatura){
+                $linhafatura->id_bebida = $linhafatura->bebida->nome;
+            }
+
+            return $linhasfatura;
+        }
+        
+        return null;
+    }
+}
